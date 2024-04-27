39a4a61a91f07a022b319f2a2e368acada05a9b9
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-27 14:41:10 +0300

    Add messages table

--- messenger/src/main/java/org/anonmes/messenger/model/Message.java
+++ messenger/src/main/java/org/anonmes/messenger/model/Message.java
@@ -1,13 +1,22 @@
 package org.anonmes.messenger.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 @Data
+@Entity
+@Table(name="messages")
 public class Message {
+    @Id
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "from_user")
     private User from;
+    @ManyToOne
+    @JoinColumn(name = "to_user")
     private User to;
     private String content;
+    @Column(name="created_at")
     private LocalDateTime createdAt;
 }
