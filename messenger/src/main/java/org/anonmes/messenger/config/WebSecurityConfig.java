3184e492205b7e728c64b321f5af195435bb781a
Yaroslav Kishchuk <98614059+kshchuk@users.noreply.github.com>
2024-04-28 10:46:29 +0300

    csrf disabled

--- messenger/src/main/java/org/anonmes/messenger/config/WebSecurityConfig.java
+++ messenger/src/main/java/org/anonmes/messenger/config/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -42,6 +43,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/**").permitAll();
                     auth.anyRequest().authenticated();
