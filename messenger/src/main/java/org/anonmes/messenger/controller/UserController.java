73fe0e44e80861e7c9eeb38574993d7dcc7fce42
TymofiiPys <nfstimoha2004@gmail.com>
2024-04-28 17:23:40 +0300

    Document UserController, set docs path to localhost:8080/api-docs.html

--- messenger/src/main/java/org/anonmes/messenger/controller/UserController.java
+++ messenger/src/main/java/org/anonmes/messenger/controller/UserController.java
@@ -1,5 +1,12 @@
 package org.anonmes.messenger.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.anonmes.messenger.dto.UserCreateDTO;
 import org.anonmes.messenger.dto.UserResponseDTO;
@@ -11,28 +18,63 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users") //or some other mapping
+@RequestMapping("/users")
+@Tag(name = "Users", description = "Users API")
 public class UserController {
     private final UserService userService;
 
+    @Operation(
+            summary = "Create a new user",
+            description = "Create a new user, given their name, email and password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDTO.class))})
+    })
     @PostMapping
-    public UserResponseDTO createUser(@RequestBody UserCreateDTO user) {
+    public UserResponseDTO createUser(@Parameter(description = "user to be added")
+                                      @RequestBody UserCreateDTO user) {
         return userService.save(user);
     }
 
+    @Operation(
+            summary = "Get all users",
+            description = "Get list of all of the users of the messenger")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
     @GetMapping
     public List<UserResponseDTO> getAllUsers() {
         return userService.getAll();
     }
 
     // TODO (optional): cases when the user deletion is unsuccessful
+    @Operation(
+            summary = "Delete user",
+            description = "Delete user, given their id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "400", description = "invalid id supplied"),
+            @ApiResponse(responseCode = "404", description = "user not found")
+    })
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.delete(id);
     }
 
+    @Operation(
+            summary = "Update user's name",
+            description = "Update name of the user, given their id and new username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "invalid id supplied"),
+            @ApiResponse(responseCode = "404", description = "user not found")
+    })
     @PutMapping
-    public UserResponseDTO updateUser(@RequestBody UserUpdateNameDTO user) {
+    public UserResponseDTO updateUser(@Parameter(description = "user to be updated with a new name")
+                                      @RequestBody UserUpdateNameDTO user) {
         return userService.update(user);
     }
 }
