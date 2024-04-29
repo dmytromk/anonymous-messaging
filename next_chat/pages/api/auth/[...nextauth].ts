1e9007678e158a9b51f2dd94ab086f673b96fdac
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-04-30 00:42:02 +0300

    fix auth with google missing token

--- next_chat/pages/api/auth/[...nextauth].ts
+++ next_chat/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,17 @@
-import NextAuth from "next-auth"
+import NextAuth, {NextAuthOptions} from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-
-
+import {NextApiRequest, NextApiResponse} from "next";
+import {serialize} from "cookie";
 const googleClient = process.env.GOOGLE_CLIENT_ID!
 const googleSecret = process.env.GOOGLE_CLIENT_SECRET!
-export default NextAuth({
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    return NextAuth(req, res, {
     providers: [
         GoogleProvider({
             clientId: googleClient,
             clientSecret: googleSecret
-        })
+        }),
     ],
     session: {
       strategy: 'jwt'
@@ -18,29 +20,30 @@ export default NextAuth({
         async jwt({ token, account, user }) {
             if (account && user) {
                 token.accessToken = account.access_token;
-                token.refreshToken = account.refresh_token!;
+                token.refreshToken = account.refresh_token;
                 token.user = {
                     id: user.id,
                     name: user.name,
                     email: user.email,
                     image: user.image
                 };
+
+                res.setHeader('Set-Cookie',
+                    serialize('anon_access_token', String(account.access_token), {
+                        httpOnly: true,
+                        secure: process.env.NODE_ENV !== 'development',
+                        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+                        path: '/'
+                    }));
+                res.redirect('/messages');
             }
-            console.log("Token:" + JSON.stringify(token))
+
             return token;
         },
     },
-
-    cookies: {
-        sessionToken: {
-            name: `next-auth.session-token`,
-            options: {
-                httpOnly: true,
-                sameSite: 'lax',
-                path: '/',
-                secure: process.env.NODE_ENV === 'production',
-            }
-        }
-    },
     secret: process.env.NEXTAUTH_SECRET,
-});
+}
+)};
+
+
+
